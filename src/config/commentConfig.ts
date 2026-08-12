54c296e61d1eeccaf2552577770553277d7c4b42
ylgyyy <97461255@qq.com>
2026-08-12 16:09:06 +0800

    更新博客配置

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -6,7 +6,7 @@ export const commentConfig: CommentConfig = {
 
 	//twikoo评论系统配置，版本1.7.4
 	twikoo: {
-		envId: "https://twikoo.trrr.top",
+		envId: "https://twikoo.887898.xyz",
 		// 设置 Twikoo 评论系统语言
 		lang: "zh-CN",
 		// 是否启用文章访问量统计功能
